2ba39e80230defa5518423c17efa290687165e9d
ayoub3bidi <mrayoubabidi@gmail.com>
2022-05-03 10:50:59 +0100

    ✨ Configuration

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBDpoUa897Fsa2TdRmQW1ZXVbSFmYjhnE8",
+    authDomain: "ommek-sannefa.firebaseapp.com",
+    projectId: "ommek-sannefa",
+    storageBucket: "ommek-sannefa.appspot.com",
+    messagingSenderId: "402999544540",
+    appId: "1:402999544540:web:9e84db560f955ea3b10219"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export {timestamp}
+export default firebaseApp.firestore();
