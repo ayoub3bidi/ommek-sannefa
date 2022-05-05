7d9e77a203ae61dda15aefb30df330769518531c
ayoub3bidi <mrayoubabidi@gmail.com>
2022-05-05 16:40:21 +0100

    ✅ Adding recipe cards

--- src/App.vue
+++ src/App.vue
@@ -56,4 +56,26 @@ export default {
 .link-light {
   color: #fff;
 }
+.card-wrap {
+  position: relative;
+  padding: 80px 16px;
+  background-color: #f1f1f1;
+  @media (min-width: 500px) {
+    padding: 100px 16px;
+  }
+  .cards {
+    display: grid;
+    gap: 32px;
+    grid-template-columns: 1fr;
+    @media (min-width: 500px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    @media (min-width: 900px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+    @media (min-width: 1200px) {
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,24 @@
   <div class="home">
     <recipePost :post="welcomeScreen"/>
     <recipePost :post="post" v-for="(post, index) in sampleRecipePost" :key="index"/>
+    <div class="card-wrap">
+      <div class="container">
+        <h3>View More Recent Posts</h3>
+        <div class="cards">
+          <recipeCard :post="post" v-for="(post, index) in sampleRecipeCards" :key="index"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import recipePost from '../components/recipePost.vue'
+import recipeCard from '../components/recipeCard.vue'
 
 export default {
   name: "Home",
-  components: {recipePost},
+  components: {recipePost, recipeCard},
   data() {
     return {
       welcomeScreen: {
@@ -32,7 +41,22 @@ export default {
           cover: "https://images.unsplash.com/photo-1587831990711-23ca6441447b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8ZGVza3RvcCUyMGNvbXB1dGVyfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
         },
       ],
+      sampleRecipeCards: [
+        { recipeTitle: "Bachkoutou", cover: "https://www.tunisienumerique.com/wp-content/uploads/2020/04/Biscuit-tunisien-%C2%ABBachkoutou%C2%BB-1200x1161.png", date: "March 23, 2022"  },
+        { recipeTitle: "Ma9roudh", cover: "https://sp-ao.shortpixel.ai/client/q_lqip,ret_wait/https://howto-cook.net/wp-content/uploads/2020/05/makroudh-recipe-for-beginners-780x517.png", date: "March 24, 2022"  },
+        { recipeTitle: "Debla", cover: "https://upload.wikimedia.org/wikipedia/commons/2/24/Oreilles_de_juge.jpg", date: "March 25, 2022"  },
+      ]
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+</style>
