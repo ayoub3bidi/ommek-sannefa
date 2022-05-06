f8f5aaafe2f717f6d39a270071d4ceae92af4ee9
ayoub3bidi <mrayoubabidi@gmail.com>
2022-05-06 12:11:05 +0100

    ✅ Adding register section

--- src/App.vue
+++ src/App.vue
@@ -27,34 +27,90 @@ export default {
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap");
-
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: "Quicksand", sans-serif;
 }
-
 .app {
   display: flex;
   flex-direction: column;
   min-height: 100vh;
 }
-
 .container {
   max-width: 1440px;
   margin: 0 auto;
 }
-
 .link {
   cursor: pointer;
   text-decoration: none;
   text-transform: uppercase;
   color: black;
 }
-
 .link-light {
   color: #fff;
+}
+.arrow {
+  margin-left: 8px;
+  width: 12px;
+  path {
+    fill: #000;
+  }
+}
+.arrow-light {
+  path {
+    fill: #fff;
+  }
+}
+button,
+.router-button {
+  transition: 500ms ease all;
+  cursor: pointer;
+  margin-top: 24px;
+  padding: 12px 24px;
+  background-color: #303030;
+  color: #fff;
+  border-radius: 20px;
+  border: none;
+  text-transform: uppercase;
+  &:focus {
+    outline: none;
+  }
+  &:hover {
+    background-color: rgba(48, 48, 48, 0.7);
+  }
+}
+.button-ghost {
+  color: #000;
+  padding: 0;
+  border-radius: 0;
+  margin-top: 50px;
+  font-size: 15px;
+  font-weight: 500;
+  background-color: transparent;
+  @media (min-width: 700px) {
+    margin-top: 0;
+    margin-left: auto;
+  }
+  i {
+    margin-left: 8px;
+  }
+}
+.button-light {
+  background-color: transparent;
+  border: 2px solid #fff;
+  color: #fff;
+}
+.button-inactive {
+  pointer-events: none !important;
+  cursor: none !important;
+  background-color: rgba(128, 128, 128, 0.5) !important;
+}
+.error {
+  text-align: center;
+  font-size: 12px;
+  color: red;
 }
 .card-wrap {
   position: relative;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,16 +10,25 @@
         </div>
       </div>
     </div>
+    <div class="updates">
+      <div class="container">
+        <h2>Never miss a post. Register for your free account today!</h2>
+        <router-link class="router-button" to="#">
+          Register <Arrow class="arrow arrow-light"/>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import recipePost from '../components/recipePost.vue'
 import recipeCard from '../components/recipeCard.vue'
+import Arrow from "../assets/Icons/arrow-right-light.svg"
 
 export default {
   name: "Home",
-  components: {recipePost, recipeCard},
+  components: {recipePost, recipeCard, Arrow},
   data() {
     return {
       welcomeScreen: {
@@ -57,6 +66,38 @@ export default {
     font-weight: 300;
     font-size: 28px;
     margin-bottom: 32px;
+  }
+}
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+      }
+    }
   }
 }
 </style>
