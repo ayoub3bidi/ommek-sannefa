ce98231cf0b8bfcb92fc21ae654b504c855a2c3d
ayoub3bidi <mrayoubabidi@gmail.com>
2022-05-08 12:01:54 +0100

    ✅ Auth implementation (#4): ForgotPassword

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,13 +1,76 @@
 <template>
-  <h1>forgotPassword</h1>
+  <div class="reset-password">
+    <Modal v-if="modalActive" :modalMessage="modalMessage" @close-modal="closeModal" />
+    <Loading v-if="loading" />
+    <div class="form-wrap">
+      <form class="reset">
+        <p class="login-register">
+          Back to
+          <router-link class="router-link" :to="{ name: 'Login' }">Login</router-link>
+        </p>
+        <h2>Reset Password</h2>
+        <p>Forgot your passowrd? Enter your email to reset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email" />
+            <email class="icon" />
+          </div>
+        </div>
+        <button @click.prevent="resetPassword">Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import email from "../assets/Icons/envelope-regular.svg";
+import Modal from "../components/Modal";
+import Loading from "../components/Loading";
 export default {
-
-}
+  name: "ForgotPassword",
+  data() {
+    return {
+      email: "",
+      modalActive: false,
+      modalMessage: "",
+      loading: null,
+    };
+  },
+  components: {
+    email,
+    Modal,
+    Loading,
+  },
+  methods: {
+    resetPassword() {
+      this.loading = true;
+          this.modalMessage = "If your account exists, you will receive a email";
+          this.loading = false;
+          this.modalActive = true;
+    },
+    closeModal() {
+      this.modalActive = !this.modalActive;
+      this.email = "";
+    },
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.reset-password {
+  position: relative;
+  .form-wrap {
+    .reset {
+      h2 {
+        margin-bottom: 8px;
+      }
+      p {
+        text-align: center;
+        margin-bottom: 32px;
+      }
+    }
+  }
+}
 </style>
