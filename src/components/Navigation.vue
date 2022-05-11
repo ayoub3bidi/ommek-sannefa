9dd4e627dcd6e6ef0eaf26c38045880c34a30f39
ayoub3bidi <mrayoubabidi@gmail.com>
2022-05-11 13:09:11 +0100

    ✅ Adding profile menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -11,6 +11,37 @@
           <router-link class="link" to="#">Create Recipe</router-link>
           <router-link class="link" :to="{ name: 'Login' }">Login/Register</router-link>
         </ul>
+        <div :class="{ 'mobile-user-menu': mobile }" @click="toggleProfileMenu" class="profile" ref="profile">
+          <span>{{ this.$store.state.profileInitials }}</span>
+          <div v-show="profileMenu" class="profile-menu">
+            <div class="info">
+              <p class="initials">{{ this.$store.state.profileInitials }}</p>
+              <div class="right">
+                <p>{{ this.$store.state.profileFirstName }} {{ this.$store.state.profileLastName }}</p>
+                <p>{{ this.$store.state.profileUsername }}</p>
+                <p>{{ this.$store.state.profileEmail }}</p>
+              </div>
+            </div>
+            <div class="options">
+              <div class="option">
+                <router-link class="option" :to="{ name: 'Profile' }">
+                  <userIcon class="icon" />
+                  <p>Profile</p>
+                </router-link>
+              </div>
+              <div class="option">
+                <router-link class="option" :to="{ name: 'Admin' }">
+                  <adminIcon class="icon" />
+                  <p>Admin</p>
+                </router-link>
+              </div>
+              <div @click="signOut" class="option">
+                <signOutIcon class="icon" />
+                <p>Sign Out</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
     <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile" />
@@ -18,7 +49,7 @@
       <ul class="mobile-nav" v-show="mobileNav">
         <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
         <router-link class="link" :to="{ name: 'Recipes' }">Recipes</router-link>
-        <router-link class="link" to="#">Create recipe</router-link>
+        <router-link class="link" to="#">Create Recipe</router-link>
         <router-link class="link" :to="{ name: 'Login' }">Login/Register</router-link>
       </ul>
     </transition>
@@ -27,11 +58,19 @@
 
 <script>
 import menuIcon from "../assets/Icons/bars-regular.svg";
+import userIcon from "../assets/Icons/user-alt-light.svg";
+import adminIcon from "../assets/Icons/user-crown-light.svg";
+import signOutIcon from "../assets/Icons/sign-out-alt-regular.svg";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 export default {
   name: "navigation",
   components: {
     menuIcon,
+    userIcon,
+    adminIcon,
+    signOutIcon,
   },
   data() {
     return {
@@ -60,7 +99,19 @@ export default {
     toggleMobileNav() {
       this.mobileNav = !this.mobileNav;
     },
+
+    toggleProfileMenu(e) {
+      if (e.target === this.$refs.profile) {
+        this.profileMenu = !this.profileMenu;
+      }
+    },
+
+    signOut() {
+      firebase.auth().signOut();
+      window.location.reload();
+    },
   },
+  computed: {},
 };
 </script>
 
@@ -116,6 +167,88 @@ header {
         }
       }
 
+      .profile {
+        position: relative;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        color: #fff;
+        background-color: #303030;
+
+        span {
+          pointer-events: none;
+        }
+
+        .profile-menu {
+          position: absolute;
+          top: 60px;
+          right: 0;
+          width: 250px;
+          background-color: #303030;
+          box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+          .info {
+            display: flex;
+            align-items: center;
+            padding: 15px;
+            border-bottom: 1px solid #fff;
+
+            .initials {
+              position: initial;
+              width: 40px;
+              height: 40px;
+              background-color: #fff;
+              color: #303030;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              border-radius: 50%;
+            }
+
+            .right {
+              flex: 1;
+              margin-left: 24px;
+
+              p:nth-child(1) {
+                font-size: 14px;
+              }
+
+              p:nth-child(2),
+              p:nth-child(3) {
+                font-size: 12px;
+              }
+            }
+          }
+
+          .options {
+            padding: 15px;
+            .option {
+              text-decoration: none;
+              color: #fff;
+              display: flex;
+              align-items: center;
+              margin-bottom: 12px;
+
+              .icon {
+                width: 18px;
+                height: auto;
+              }
+              p {
+                font-size: 14px;
+                margin-left: 12px;
+              }
+
+              &:last-child {
+                margin-bottom: 0px;
+              }
+            }
+          }
+        }
+      }
     }
 
     .mobile-user-menu {
